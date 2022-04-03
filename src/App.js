6401ff518fe924e758255cbf40c44092236c9efa
Farhan <farhansafin@gmail.com>
2022-04-03 20:56:53 +0600

    NavBar Basic Done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
     <Header></Header>
-      <h1 className='text-4xl'>Product Analysis</h1>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from '../Link/Link';
 
 const Navbar = () => {
+
     const routes = [
         {id: 1, name: 'Home', link: '/home'},
         {id: 2, name: 'Reviews', link: '/reviews'},
@@ -9,9 +10,10 @@ const Navbar = () => {
         {id: 4, name: 'Blogs', link: '/blogs'},
         {id: 5, name: 'About', link: '/about'},
     ]
+
     return (
         <nav>
-            <ul>
+            <ul className='md:flex justify-center'>
                 {
                     routes.map(route => <Link 
                     key={route.id}
@@ -20,6 +22,13 @@ const Navbar = () => {
                 }
             </ul>
         </nav>
+
+ 
+
+
+
+
+
     );
 };
 
