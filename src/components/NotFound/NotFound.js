a59be0029b0ba8f763fc11856c2a2a62289d9770
Farhan <farhansafin@gmail.com>
2022-04-05 01:13:05 +0600

    Fixed everything. Font added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -6,14 +6,15 @@ const NotFound = () => {
     return (
         <div className='mt-60'>
             <h1 className='text-4xl'>Page Not Found</h1>
-<section className="error-container">
-  <span><span>4</span></span>
-  <span>0</span>
-  <span><span>4</span></span>
-</section>
-<div className='link-container'>
-  <Link to="/home">Return to Home</Link>
-</div>
+            <section className="error-container">
+              <span><span>4</span></span>
+              <span>0</span>
+              <span><span>4</span></span>
+            </section>
+            
+            <div className='link-container'>
+              <Link to="/home">Return to Home</Link>
+            </div>
         </div>
     );
 };
