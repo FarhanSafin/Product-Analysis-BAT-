b5e7c26d01c5aa1f2c1d9eac8f242114f5ade526
Farhan <farhansafin@gmail.com>
2022-04-04 14:19:32 +0600

    About us page done

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,10 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h2>About</h2>
+            <h2 className='text-4xl mt-72 underline hover:uppercase'>We are the Bat Seller</h2>
+            <p className='text-2xl mt-10'>From us you can get the best bat for playing the game you love.</p>
+            <p className='text-2xl mt-16'>If you want to contact us-</p>
+            <p className='text-1xl mt-8 underline decoration-4 hover:font-bold'>bat-analyser@outlook.com</p>
         </div>
     );
 };
