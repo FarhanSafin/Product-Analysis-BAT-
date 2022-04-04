cf912273c31328c70829e596d96a225484807cb4
Farhan <farhansafin@gmail.com>
2022-04-04 13:58:36 +0600

    Not found page done

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>Nothing here: 404</h2>
+        <div className='mt-60'>
+            <h1 className='text-4xl'>Page Not Found</h1>
+<section class="error-container">
+  <span><span>4</span></span>
+  <span>0</span>
+  <span><span>4</span></span>
+</section>
+<div class="link-container">
+  <Link to="/home">Return to Home</Link>
+</div>
         </div>
     );
 };
